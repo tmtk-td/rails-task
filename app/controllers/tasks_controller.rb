7e2821cf4d3657bc21e004d37f586168355d903c
tmtk <blindhurts.6001@gmail.com>
2020-03-27 18:34:31 +0900

    deteil-create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,9 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+  def show
+    @task = Task.find(params[:id])
+  end
   def create
      Task.create(params.require(:task).permit(:content))
      redirect_to new_task_path
