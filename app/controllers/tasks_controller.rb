edee7631415bb21c82a0e4a64cc93974ef1eeed9
tmtk <blindhurts.6001@gmail.com>
2020-03-27 18:06:23 +0900

    indexcreate

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,12 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
   def new
+    @task = Task.new
+  end
+  def create
+     Task.create(params.require(:task).permit(:content))
+     redirect_to new_task_path
   end
 end
