5988a4d3b4a3235129da2af89fd98e65cb0ccf9a
tmtk <blindhurts.6001@gmail.com>
2020-03-30 21:12:25 +0900

    complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,30 +1,31 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
   def new
     @task = Task.new
   end
-  def show
-    @task = Task.find(params[:id])
-  end
   def create
-    Task.create(blog_params)
-    redirect_to new_blog_path
+    Task.create(task_params)
+    redirect_to new_task_path
   end
   def create
     @task = Task.new(task_params)
+    if params[:back]
+      render :new
+    else
     if @task.save
       # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
-      redirect_to blogs_path, notice: "ブログを作成しました！"
+      redirect_to tasks_path, notice: "お言葉を作成しました"
     else
       # 入力フォームを再描画します。
       render :new
     end
+   end
   end
   def show
-   # @task = Task.find(params[:id])
+    # @blog = Blog.find(params[:id])
   end
   def edit
     # @task = Task.find(params[:id])
@@ -32,16 +33,24 @@ class TasksController < ApplicationController
   def update
     # @task = Task.find(params[:id])
     if @task.update(task_params)
-      redirect_to tasks_path, notice: "ブログを編集しました！"
+      redirect_to tasks_path, notice: "お言葉を編集しました"
     else
       render :edit
     end
+  end
+    def destroy
+    @task.destroy
+    redirect_to tasks_path, notice:"お言葉を削除しました"
+  end
+  def confirm
+    @task = Task.new(task_params)
+     render :new if @task.invalid?
   end
   private
   def task_params
     params.require(:task).permit(:content)
   end
-  def set_blog
+  def set_task
     @task = Task.find(params[:id])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # get '/tasks', to: 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tasks
+  resources :tasks do
+     collection do
+      post :confirm
+    end
+  end  
 end
